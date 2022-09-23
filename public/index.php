6d4eb49701c922a518c7c855d06d20890c32cf0f
PG-Momik <momik12339@gmail.com>
2022-09-23 10:42:03 +0545

    index updated

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
 
-//entry point
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\controllers\AuthController;
+use app\controllers\SiteController;
+use app\core\Application;
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$rootPath = dirname(__DIR__);
+$app = new Application(dirname(__DIR__));
+
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
+
+
+$app->run();
